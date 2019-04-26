f9651f1587f6741432f0bdaf7acbbb2b7efb071c
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2019-04-26 20:02:41 +0200

    extended test

--- test_package/osdialogtest.cpp
+++ test_package/osdialogtest.cpp
@@ -2,5 +2,11 @@
 
 int main() {
 
+   char* pattern = "pattern";
+   char* filter = "filter";
+
+   osdialog_filter_patterns dialogPattern = {pattern, nullptr};
+   osdialog_filters dialogFilter = {filter, &dialogPattern, nullptr};
+
    return 0;
 }
