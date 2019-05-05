fff41087fdcdccec153089010366295668e1bd35
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2019-05-05 09:54:49 +0200

    fix libcxx and fPIC problems

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ os.environ["CONAN_CHANNEL"]                 = "testing"
 
 
 def _is_static_msvc_build(build):
-  if build.options["OSDialog:shared"] == True and build.settings["compiler"] != "Visual Studio":
+  if build.options["OSDialog:shared"] == True:
     return False
   else:
     return True
